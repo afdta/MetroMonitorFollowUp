d63215c4fd2150c72af245943da6a5c175f7c8e7
Alec Friedhoff <afriedhoff@gmail.com>
2015-12-09 13:19:18 -0500

    Working out how to structure the app so it is modular. Need to keep track of (1) View state and (2) Metro state. Questions... Should tracking be hierarchical or flat? How fine-grained should the metro listeners be? I.e. should I create individual listeners for each visual within each view or should I just handle the view with one listener. The latter is a much simpler solution because then there's really only an array of view listners... One listener for each view that updates the view based on the selected metro. What about sub-views with in each view? What does the overview of the core data view look like?

--- src/state-tracking.js
+++ src/state-tracking.js
@@ -1,26 +1,38 @@
-//produce an object for geography state tracking
-function MetroState(){
+//produce an object for metro index state tracking
+function MetroIndexState(){
 	var S = {};
+
 	//keep track of selected metro area code
-	S.metro = null;
+	S.metro = null; //no defaults
+	S.view = null; 
+
+	//metro lookup table to validate metro areas
+	S.metroLookup = null;
 	
-	//add listeners that will be called upon changes to metro code
+	//each view listener entry needs to be structured accordingly:
+	//
+	S.viewListeners = {};
+
+	//metro listeners that will be called upon changes to metro code
 	S.metListeners = [];
 	S.addMetroChangeListener = function(f){
 		S.metListeners.push(f);
 	}
 
-	S.metroChange = function(metroCode, thiz){
+	//args
+	//{1} metroCode: metro code to change to. if not a valid code, nothing happens. 
+	//{2} setHash: optional, if truthy, this method will also set the page hash value to metroCode (assuming valid metro code)
+	S.metroChange = function(metroCode, setHash){
 		//validate
 		if(metroCode in {}){
+			S.metro = metroCode;
+			if(!!setHash){set_hash(metroCode);}
 
-		}
+			//execute listeners
+			for(var i=0; i<S.metListeners.length; i++){
+				S.metListeners[i]();
+			}
 
-		if(!thiz){
-			for
-		}
-		else{
-			//call with context
 		}
 	}
 
@@ -47,12 +59,11 @@ function MetroState(){
 	function hash_listener(){
 		var h = get_hash(); 
 		if(h != S.metro){
-			S.metroChange(h); //perform metro code validation in metroChange
+			S.metroChange(h); //metro code validation performed in metroChange
 		}
 	}
 
 	window.addEventListener("hashchange",hash_listener,false)
-
 
 	return S;
 };
