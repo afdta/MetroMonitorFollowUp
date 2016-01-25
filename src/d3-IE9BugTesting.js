5af77c2f4ba7bd1465ae5daabd7d822ae0aac20d
afdta <afriedhoff@live.com>
2016-01-24 21:13:30 -0500

    Reverting stack trace

--- src/d3-IE9BugTesting.js
+++ src/d3-IE9BugTesting.js
@@ -1,56 +1,3 @@
-function printStackTrace() {
-  var callstack = [];
-  var isCallstackPopulated = false;
-  try {
-    i.dont.exist+=0; //doesn't exist- that's the point
-  } catch(e) {
-    if (e.stack) { //Firefox
-      var lines = e.stack.split('\n');
-      for (var i=0, len=lines.length; i&lt;len; i++) {
-        if (lines[i].match(/^\s*[A-Za-z0-9\-_\$]+\(/)) {
-          callstack.push(lines[i]);
-        }
-      }
-      //Remove call to printStackTrace()
-      callstack.shift();
-      isCallstackPopulated = true;
-    }
-    else if (window.opera &amp;&amp; e.message) { //Opera
-      var lines = e.message.split('\n');
-      for (var i=0, len=lines.length; i&lt;len; i++) {
-        if (lines[i].match(/^\s*[A-Za-z0-9\-_\$]+\(/)) {
-          var entry = lines[i];
-          //Append next line also since it has the file info
-          if (lines[i+1]) {
-            entry += ' at ' + lines[i+1];
-            i++;
-          }
-          callstack.push(entry);
-        }
-      }
-      //Remove call to printStackTrace()
-      callstack.shift();
-      isCallstackPopulated = true;
-    }
-  }
-  if (!isCallstackPopulated) { //IE and Safari
-    var currentFunction = arguments.callee.caller;
-    while (currentFunction) {
-      var fn = currentFunction.toString();
-      var fname = fn.substring(fn.indexOf(&amp;quot;function&amp;quot;) + 8, fn.indexOf('')) || 'anonymous';
-      callstack.push(fname);
-      currentFunction = currentFunction.caller;
-    }
-  }
-  output(callstack);
-}
-
-function output(arr) {
-  //Optput however you want
-  alert(arr.join('\n\n'));
-};
-
-
 !function() {
   var d3 = {
     version: "3.5.11"
@@ -95,7 +42,6 @@ function output(arr) {
           d3_style_setProperty.call(this, name, value + "", priority);
         }
         catch(e){
-          printStackTrace();
           console.log(e);
           console.log("name: "+name+" | value: "+value+" | priority: "+priority);
         }
